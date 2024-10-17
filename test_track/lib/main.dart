da7ff16cbe4b5c60ae1daa893f2c85420ec8aa1e
Josephplaugher-HDR <joseph.plaugher@hdrinc.com>
2024-10-17 15:44:20 -0700

    added .env

--- test_track/lib/main.dart
+++ test_track/lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:arcgis_maps/arcgis_maps.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:io';
 
-void main() {
-  // replace 'YOUR_ACCESS_TOKEN' with your API key access token
-  ArcGISEnvironment.apiKey =
-      'AAPTxy8BH1VEsoebNVZXo8HurJvjrCRhQLxEcvFJ54L52yfMF8SqJXkXpFqBfRjc8xa3qlVHHeo0NKu2IdVfnAdkIy5OxDDSEGi19Dui-vcUlzbZ9JVlzt2pgC-CdM0fO2wcJnQjKx9SFpgNoUp41tu_VTm27bO5YFctaAD1YAuQu-DIDufPXRGM3XGvWVL-etkxhvQp4zzcLtasImfG2keRsTQCaLuleK8O1uQBPwqVmJGziZuS5IewJcyiyu0s1YU1AT1_qz81fJDD';
-
-  runApp(const MaterialApp(home: MyApp()));
+void main() async {
+  print("we are here: " + Directory.current.path);
+  await dotenv.load(fileName: ".env");
+  ArcGISEnvironment.apiKey = dotenv.env["ESRI_KEY"] ?? "No API key found";
+  runApp(const MaterialApp(home: App()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
